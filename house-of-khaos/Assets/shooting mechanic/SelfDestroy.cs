999a46ae6934abb0cabf46c55015ccaf0d405bca
Raigir <Hammontreeryan@gmail.com>
2015-01-26 19:18:06 -0600

    shooting works
    Bug: Bullets don't collide with the floor?!

---
+++ house-of-khaos/Assets/shooting mechanic/SelfDestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy (gameObject, 5f);
+	}
+
+}
---
+++ house-of-khaos/Assets/shooting mechanic/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public float shotForce = 1000f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonUp("Fire1"))
+		{
+			Rigidbody shot = Instantiate(projectile, this.transform.position, this.transform.parent.rotation) as Rigidbody;
+			shot.rigidbody.AddForce(shot.transform.forward * shotForce);
+		}
+	}
+}
