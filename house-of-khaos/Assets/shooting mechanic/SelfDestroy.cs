185420f07881d146a89a76faa4674791d05808ca
Kenneth Smith <kwgsmith@gmail.com>
2015-01-27 08:20:01 -0600

    Level created, shooting fixed
    Increased size of bullet, shooting seems to be fixed/more obvious now.
    Created a quick level with 4 walls to box us in.

--- house-of-khaos/Assets/shooting mechanic/SelfDestroy.cs
+++ house-of-khaos/Assets/shooting mechanic/SelfDestroy.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class SelfDestroy : MonoBehaviour {
 
+	public float time = 1f;
+
 	// Use this for initialization
 	void Start () 
 	{
-		Destroy (gameObject, 5f);
+		Destroy (gameObject, time);
 	}
 
 }
