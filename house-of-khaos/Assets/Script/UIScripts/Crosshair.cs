fb22f199c5a862b3eef8911653069979737f1ff7
Steven Burke <tsunayoshi1234@gmail.com>
2015-01-29 18:49:36 -0600

    First part of Onlook

---
+++ house-of-khaos/Assets/Script/UIScripts/Crosshair.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+	public RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2,Screen.height/2, 0));
+		if(Physics.Raycast(ray, out hit, 5)){
+				hit.collider.gameObject.GetComponent<Interaction>().OnLookEnter();
+		}
+	}
+}
