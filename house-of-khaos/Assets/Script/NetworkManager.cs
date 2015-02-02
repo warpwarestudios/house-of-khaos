45f5dd9fd8c587b351e5bd4626d8441e86e575c6
Raigir <Hammontreeryan@gmail.com>
2015-02-01 22:09:24 -0600

    cleaned up files, preliminary multi
    LOTS OF BUGS

---
+++ house-of-khaos/Assets/Script/NetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+		//PhotonNetwork.logLevel = PhotonLogLevel.Full;
+		PhotonNetwork.ConnectUsingSettings("0.01");
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	// call back for succesful lobby join
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	// call back on succesful room join
+	void OnJoinedRoom()
+	{
+		GameObject player = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+	}
+
+	// call back for failed to join
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room!");
+		PhotonNetwork.CreateRoom(null);
+	}
+}
