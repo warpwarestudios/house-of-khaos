ccdd020e9bd16192f4d5b2456f9536ab53029734
Kenneth Smith <kwgsmith@gmail.com>
2015-02-09 17:52:13 -0600

    Working on randomization
    Starting basic room size randomization

---
+++ house-of-khaos/Assets/Script/Randomization/MapGeneration.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+
+	public GameObject room;
+	public bool generate;
+
+	// Use this for initialization
+	void Start () {
+		generate = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (generate) 
+		{
+			Destroy(GameObject.Find ("Room(Clone)"));
+			GameObject newRoom = (GameObject)Instantiate(room, new Vector3(0,0,0), Quaternion.identity);
+			this.transform.parent = newRoom.transform;
+			Vector3 scale = new Vector3(Random.Range(5,20),0,Random.Range(5,20));
+			room.transform.localScale = scale;
+			generate = false;
+		}
+	}
+
+}
