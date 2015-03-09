75c63d6e5f3d42a2209480ceb3db11dfbb20889d
Steven Burke <tsunayoshi1234@gmail.com>
2015-03-08 23:00:02 -0600

    Moar commits

---
+++ house-of-khaos/Assets/Script/UITitleManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITitleManager : MonoBehaviour {
+
+	public GameObject titleCamera;
+	public GameObject lobbyCamera;
+	
+	void Start() 
+	{
+		titleCamera.SetActive(true);
+		lobbyCamera.SetActive(false);
+	}
+	
+	public void SwitchToLobby()
+	{
+		titleCamera.SetActive(false);
+		lobbyCamera.SetActive (true);
+	}
+	
+	public void SwitchToTitle()
+	{
+		titleCamera.SetActive(true); 
+		lobbyCamera.SetActive(false);
+	}
+	
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
