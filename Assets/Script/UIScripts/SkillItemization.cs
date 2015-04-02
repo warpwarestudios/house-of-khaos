c845f01d6c39d4d816295766ae518559544b6267
Steven Burke <tsunayoshi1234@gmail.com>
2015-04-02 16:43:57 -0500

    fix gun

--- Assets/Script/UIScripts/SkillItemization.cs
+++ Assets/Script/UIScripts/SkillItemization.cs
@@ -6,10 +6,10 @@ public class SkillItemization : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		//    GAMEOBJECT			NAME			ICONNAME			SKILLNAME			DURABILITY/SANITY/AMMO			DAMAGE					HEAL							POSITION						ROTATION						SCALE                 
-		Skill("EmptySlot",			"Empty",		"WhiteBackground",	"None",				0,								0,						0,								new Vector3(0f,0f,0f),			Quaternion.Euler(0,0,0),		new Vector3(0,0,0));
-		Skill("Gun",				"Pistol",		"BlueCube",			"Shoot",			10,								20,						0,								new Vector3(0.4f,-0.4f,1.2f), 	Quaternion.Euler(0.0f,82,0),	new Vector3(0.32f,0.27f,0.27f));
-		Skill("Rosary",				"Rosary",		"RedCube",			"None",				10,								0,						30,								new Vector3(0.4f,-0.4f,0.65f),	Quaternion.Euler(0.0f,100,0),	new Vector3(1,1,1));
+		//    GAMEOBJECT			NAME			ICONNAME			SKILLNAME			DURABILITY/SANITY/AMMO			DAMAGE					HEAL							POSITION							ROTATION						SCALE                 
+		Skill("EmptySlot",			"Empty",		"WhiteBackground",	"None",				0,								0,						0,								new Vector3(0f,0f,0f),				Quaternion.Euler(0,0,0),		new Vector3(0,0,0));
+		Skill("Gun",				"Pistol",		"BlueCube",			"Shoot",			10,								20,						0,								new Vector3(0.12f,-0.17f,0.59f), 	Quaternion.Euler(2.0f,-2f,17.5f),	new Vector3(0.32f,0.27f,0.27f));
+		Skill("Rosary",				"Rosary",		"RedCube",			"None",				10,								0,						30,								new Vector3(0.4f,-0.4f,0.65f),		Quaternion.Euler(0.0f,100,0),	new Vector3(1,1,1));
 	 	
 	}
 	
