8005a1d61752da4c4b7f599084026ad55a35ec19
Raigir <Hammontreeryan@gmail.com>
2015-04-19 17:17:04 -0500

    ZOMBIE ATTACKS!

--- Assets/AITest/EnemyAi/ZombieAI.cs
+++ Assets/AITest/EnemyAi/ZombieAI.cs
@@ -11,11 +11,10 @@ public class ZombieAI : MonoBehaviour {
 	public float chaseSpeed = 5f;// The nav mesh agent's speed when chasing.
 	public float chaseWaitTime = 5f;// The amount of time to wait when the last sighting is reached.
 	private float chaseTimer;// A timer for the chaseWaitTime.
-	public Vector3 personalLastSighting = new Vector3(1000f, 1000f, 1000f);// The last global sighting of the player.
-	public Vector3 resetPosition = new Vector3(1000f, 1000f, 1000f);// The default position if the player is not in sight.
 	
 	// Use this for initialization
 	void Start () {
+
 		// Set up the references.
 		player = GameObject.FindGameObjectWithTag ("Player");
 		navAgent = GetComponent <NavMeshAgent> ();
@@ -25,10 +24,9 @@ public class ZombieAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		//navAgent.SetDestination (player.position);
-		if ((player = GameObject.FindGameObjectWithTag ("Player"))!= null) 
+		if (player == null) 
 		{
-			personalLastSighting = player.transform.position;
+			player = GameObject.FindGameObjectWithTag ("Player");
 		}
 		else
 		{
@@ -39,7 +37,7 @@ public class ZombieAI : MonoBehaviour {
 		// If the player has been sighted and isn't dead...
 		if(player != null)
 		{
-			if (personalLastSighting != resetPosition && player.GetComponent<Health> ().currentHealth > 0f) 
+			if (player.GetComponent<Health> ().currentHealth > 0f) 
 			{
 				// ... chase.
 				Chasing();
@@ -47,51 +45,28 @@ public class ZombieAI : MonoBehaviour {
 		}
 
 		// If the player is in sight, in range and is alive...
-		/*if (navAgent.remainingDistance <= navAgent.stoppingDistance && player.GetComponent<Health> ().currentHealth > 0f) 
+		if (navAgent.hasPath && navAgent.remainingDistance <= navAgent.stoppingDistance && player.GetComponent<Health> ().currentHealth > 0f) 
 		{
-
-			StartCoroutine ("Attacking");
 			// ... attack.
-			//Attacking ();
-			animController.SetBool ("Attack", false);
-		}*/
+			Attacking ();
+		}
 
 		//set animation speed based on navAgent 'Speed' var
 		// causes issues
 		//animController.speed = navAgent.speed;
 	}
 
-	void OnAnimatorMove ()
-	{
-		//only perform if walking
-		if (navAgent.hasPath)
-		{
-			//set the navAgent's velocity to the velocity of the animation clip currently playing
-			navAgent.velocity = animController.deltaPosition / Time.deltaTime;
-			
-			//smoothly rotate the character in the desired direction of motion
-			Quaternion lookRotation = Quaternion.LookRotation(navAgent.desiredVelocity);
-			transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, navAgent.angularSpeed * Time.deltaTime);
-		}
-	}
-
-	IEnumerator Attacking ()
+	private void Attacking ()
 	{
 		// Stop the enemy where it is.
-		navAgent.SetDestination (this.transform.position);
-		animController.SetBool ("Attack", true);
-		yield return new WaitForSeconds (2.6f);
+		navAgent.Stop ();
+		animController.SetTrigger ("Attack");
 	}
 
 	private void Chasing()
 	{
-		// Create a vector from the enemy to the last sighting of the player.
-		Vector3 sightingDeltaPos = personalLastSighting - transform.position;
-		
-		// If the the last personal sighting of the player is not close...
-		//if(sightingDeltaPos.sqrMagnitude > 4f)
-		// ... set the destination for the NavMeshAgent to the last personal sighting of the player.
-		navAgent.destination = personalLastSighting;
+		// set the destination for the NavMeshAgent to the last personal sighting of the player.
+		navAgent.SetDestination (player.transform.position);
 		
 		// Set the appropriate speed for the NavMeshAgent.
 		if (player != null) 
@@ -106,7 +81,6 @@ public class ZombieAI : MonoBehaviour {
 			ChaseAnim();
 		}
 		
-		
 		// If near the last personal sighting...
 		if(navAgent.remainingDistance < navAgent.stoppingDistance)
 		{
@@ -117,7 +91,7 @@ public class ZombieAI : MonoBehaviour {
 			if(chaseTimer >= chaseWaitTime)
 			{
 				// ... reset last global sighting, the last personal sighting and the timer.
-				personalLastSighting = resetPosition;
+				player = null;
 				chaseTimer = 0f;
 				navAgent.speed = wanderSpeed;
 				IdleAnim();
@@ -126,6 +100,21 @@ public class ZombieAI : MonoBehaviour {
 		else
 			// If not near the last sighting personal sighting of the player, reset the timer.
 			chaseTimer = 0f;
+	}
+
+	// keep model turned right way
+	void OnAnimatorMove ()
+	{
+		//only perform if walking
+		if (navAgent.hasPath)
+		{
+			//set the navAgent's velocity to the velocity of the animation clip currently playing
+			navAgent.velocity = animController.deltaPosition / Time.deltaTime;
+			
+			//smoothly rotate the character in the desired direction of motion
+			Quaternion lookRotation = Quaternion.LookRotation(navAgent.desiredVelocity);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, navAgent.angularSpeed * Time.deltaTime);
+		}
 	}
 
 	private void IdleAnim()
