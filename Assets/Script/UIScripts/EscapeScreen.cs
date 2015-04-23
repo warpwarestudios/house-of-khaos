98bf535e35f6d900c35c3a01b3251f5467422f51
Steven Burke <tsunayoshi1234@gmail.com>
2015-04-23 18:21:44 -0500

    escape screen

---
+++ Assets/Script/UIScripts/EscapeScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeScreen : MonoBehaviour {
+	
+	
+	// Use this for initialization
+	void Start () {
+		this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKey(KeyCode.Escape))
+		{
+			GameObject.Find("EscapeScreen").gameObject.SetActive(true);
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		
+		}
+		else
+		{
+			GameObject.Find("EscapeScreen").gameObject.SetActive(true);
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+	}
+	
+    public void LeaveGame()
+    {
+    	PhotonNetwork.LeaveRoom();
+    	Application.LoadLevel("LobbyScreen");
+    }
+    
+    public void ExitGame()
+    {
+		PhotonNetwork.LeaveRoom();
+		Application.Quit();
+    }
+    
+}
