eec3d1226a5d5b031b9da1c5bf3b626ea96cb5e2
Steven Burke <tsunayoshi1234@gmail.com>
2015-05-05 01:02:00 -0500

    health bar added

---
+++ Assets/Script/UIScripts/HealthBarUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarUI : MonoBehaviour {
+	
+	public Health health;
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.Find("UI Root").transform.FindChild("HealthBar").GetComponent<UISlider>().value = ((health.currentHealth / health.maxHealth) / 100);
+		GameObject.Find("UI Root").transform.FindChild("HealthBar").transform.FindChild("HP").GetComponent<UILabel>().text = ((health.currentHealth / health.maxHealth) * 100) + "%";
+	}
+}
