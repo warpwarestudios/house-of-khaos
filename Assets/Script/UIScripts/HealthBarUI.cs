260116afb0ac25d64f872179b6c2eba2eae23cd1
Steven Burke <tsunayoshi1234@gmail.com>
2015-05-05 01:30:29 -0500

    last build dont ask more

--- Assets/Script/UIScripts/HealthBarUI.cs
+++ Assets/Script/UIScripts/HealthBarUI.cs
@@ -7,7 +7,7 @@ public class HealthBarUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		GameObject.Find("UI Root").transform.FindChild("HealthBar").GetComponent<UISlider>().value = ((health.currentHealth / health.maxHealth) * 100);
+		GameObject.Find("UI Root").transform.FindChild("HealthBar").GetComponent<UISlider>().value = ((health.currentHealth / health.maxHealth));
 		GameObject.Find("UI Root").transform.FindChild("HealthBar").GetComponent<UISlider>().alpha = ((health.currentHealth / health.maxHealth));
 
 		GameObject.Find("UI Root").transform.FindChild("HealthBar").transform.FindChild("HP").GetComponent<UILabel>().text = ((health.currentHealth / health.maxHealth) * 100) + "%";
